2ff4f2ef95bdca80f62df220c13dbfd91cb283b2
jmacey <jmacey@bournemouth.ac.uk>
2021-11-06 15:24:38 +0000

    updated SimpleNGL.py to work with PyQt5 and PyQt6

--- SimpleNGL.py
+++ SimpleNGL.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
-from PyQt5.QtGui import QOpenGLWindow,QSurfaceFormat
-from PyQt5.QtWidgets import QApplication
-from  PyQt5.QtCore import *
+try : # support either PyQt5 or 6
+  from PyQt5.QtGui import QOpenGLWindow,QSurfaceFormat
+  from PyQt5.QtWidgets import QApplication
+  from  PyQt5.QtCore import *
+  PyQtVersion = 5
+except ImportError:
+  print('trying Qt6')
+  from PyQt6.QtGui import QSurfaceFormat
+  from PyQt6.QtOpenGL import QOpenGLWindow
+  from PyQt6.QtWidgets import QApplication
+  from PyQt6.QtCore import QEvent,Qt
+  PyQtVersion = 6
+
 import sys
 from pyngl import *
 from OpenGL.GL import *
@@ -48,9 +58,9 @@ class MainWindow(QOpenGLWindow) :
     ShaderLib.use('PBR')
 
     # We now create our view matrix for a static camera
-    From=Vec3(0.0, 2.0, 2.0 ) 
-    to  =Vec3( 0.0, 0.0, 0.0 ) 
-    up  =Vec3( 0.0, 1.0, 0.0 ) 
+    From=Vec3(2.0, 2.0, 2.0 ) 
+    to=Vec3( 0.0, 0.0, 0.0 ) 
+    up=Vec3( 0.0, 1.0, 0.0 ) 
     # now load to our new camera
     self.view=lookAt(From,to,up) 
     self.projection=perspective( 45.0, float( self.width  / self.height), 0.5, 20.0 )
@@ -95,7 +105,6 @@ class MainWindow(QOpenGLWindow) :
     ShaderLib.setUniform('M',M)
     ShaderLib.setUniform('MVP',MVP)
     ShaderLib.setUniform('normalMatrix',normalMatrix)
-    #ShaderLib.setUniformBuffer('TransformUBO',len(ubo),ubo.MVP)
     if self.transformLight == True :
       ShaderLib.setUniform('lightPosition',(self.mouseGlobalTX*self.lightPos).toVec3())
     
@@ -127,7 +136,7 @@ class MainWindow(QOpenGLWindow) :
       VAOPrimitives.draw('floor')
 
     except OpenGL.error.GLError :
-      print 'error'
+      print( 'error')
 
   def resizeGL(self, w,h) :
     self.width=int(w* self.devicePixelRatio())
@@ -152,53 +161,147 @@ class MainWindow(QOpenGLWindow) :
     
     self.update()
 
-  def mouseMoveEvent(self, event) :
-    if self.rotate and event.buttons() == Qt.LeftButton  :
-      diffx = int(event.x() - self.origX)
-      diffy = int(event.y() - self.origY)
-      self.spinXFace += int( 0.5 * diffy )
-      self.spinYFace += int( 0.5 * diffx )
-      self.origX = event.x()
-      self.origY = event.y()
+  
+
+  if PyQtVersion == 5 :
+    def keyPressEvent(self, event) :
+      key=event.key()
+      if key==Qt.Key_Escape :
+        exit()
+      elif key==Qt.Key_W :
+        glPolygonMode(GL_FRONT_AND_BACK,GL_LINE)
+      elif key==Qt.Key_S :
+        glPolygonMode(GL_FRONT_AND_BACK,GL_FILL)
+      elif key==Qt.Key_Space :
+        self.spinXFace=0
+        self.spinYFace=0
+        self.modelPos.set(Vec3.zero())
+      elif key==Qt.Key_L :
+        self.transformLight^=True      
       self.update()
 
-    elif  self.translate and event.buttons() == Qt.RightButton :
+    def mouseMoveEvent(self, event) :
+        if self.rotate and event.buttons() == Qt.LeftButton  :
+          
+          diffx = int(event.x() - self.origX)
+          diffy = int(event.y() - self.origY)
+          self.spinXFace += int( 0.5 * diffy )
+          self.spinYFace += int( 0.5 * diffx )
+          self.origX = event.x()
+          self.origY = event.y()
+          self.update()
+        elif  self.translate and event.buttons() == Qt.RightButton :
 
-      diffX   = int( event.x() - self.origXPos )
-      diffY   = int( event.y() - self.origYPos )
-      self.origXPos = event.x()
-      self.origYPos = event.y()
-      self.modelPos.m_x += self.INCREMENT * diffX 
-      self.modelPos.m_y -= self.INCREMENT * diffY 
+          diffX   = int( event.x() - self.origXPos )
+          diffY   = int( event.y() - self.origYPos )
+          self.origXPos = event.x()
+          self.origYPos = event.y()
+          self.modelPos.m_x += self.INCREMENT * diffX 
+          self.modelPos.m_y -= self.INCREMENT * diffY 
+          self.update() 
+
+
+    def mousePressEvent(self,event) :
+        if  event.button() == Qt.LeftButton :
+          self.origX  = event.x()
+          self.origY  = event.y()
+          self.rotate = True
+
+        elif  event.button() == Qt.RightButton :
+          self.origXPos  = event.x() 
+          self.origYPos  = event.y() 
+          self.translate = True
+
+    def mouseReleaseEvent(self,event) :
+      if  event.button() == Qt.LeftButton :
+        self.rotate = False
+
+      elif  event.button() == Qt.RightButton :
+        self.translate = False
+
+    def wheelEvent(self,event) :
+      numPixels = event.pixelDelta() 
+
+      if  numPixels.x() > 0  :
+        self.modelPos.m_z += self.ZOOM
+
+      elif  numPixels.x() < 0 :
+        self.modelPos.m_z -= self.ZOOM
       self.update() 
 
-  def mousePressEvent(self,event) :
-    if  event.button() == Qt.LeftButton :
-      self.origX  = event.x()
-      self.origY  = event.y()
-      self.rotate = True
+  ##############################################################################
+  # Qt6 
+  ##############################################################################
+  else : # Qt6 Versions
 
-    elif  event.button() == Qt.RightButton :
-      self.origXPos  = event.x() 
-      self.origYPos  = event.y() 
-      self.translate = True
+    def mousePressEvent(self,event) :
+      pos = event.position()
+      if  event.button() == Qt.MouseButton.LeftButton :
+        self.origX  = pos.x()
+        self.origY  = pos.y()
+        self.rotate = True
 
-  def mouseReleaseEvent(self,event) :
-    if  event.button() == Qt.LeftButton :
-      self.rotate = False
+      elif  event.button() == Qt.MouseButton.RightButton :
+        self.origXPos  = pos.x() 
+        self.origYPos  = pos.y() 
+        self.translate = True
+    
+    def mouseMoveEvent(self, event) :
+      if self.rotate and event.buttons() == Qt.MouseButton.LeftButton  :
+        pos=event.position()
+        diffx = int(pos.x() - self.origX)
+        diffy = int(pos.y() - self.origY)
+        self.spinXFace += int( 0.5 * diffy )
+        self.spinYFace += int( 0.5 * diffx )
+        self.origX = pos.x()
+        self.origY = pos.y()
+        self.update()
+      elif  self.translate and event.buttons() == Qt.MouseButton.RightButton :
+        pos=event.position()
+        diffX   = int( pos.x() - self.origXPos )
+        diffY   = int( pos.y() - self.origYPos )
+        self.origXPos = pos.x()
+        self.origYPos = pos.y()
+        self.modelPos.m_x += self.INCREMENT * diffX 
+        self.modelPos.m_y -= self.INCREMENT * diffY 
+        self.update() 
 
-    elif  event.button() == Qt.RightButton :
-      self.translate = False
+    def mouseReleaseEvent(self,event) :
+      if  event.button() == Qt.MouseButton.LeftButton :
+        self.rotate = False
 
-  def wheelEvent(self,event) :
-    numPixels = event.pixelDelta() 
+      elif  event.button() == Qt.MouseButton.RightButton :
+        self.translate = False
 
-    if  numPixels.x() > 0  :
-      self.modelPos.m_z += self.ZOOM
+    def wheelEvent(self,event) :
+      numPixels = event.pixelDelta() 
 
-    elif  numPixels.x() < 0 :
-      self.modelPos.m_z -= self.ZOOM
-    self.update() 
+      if  numPixels.x() > 0  :
+        self.modelPos.m_z += self.ZOOM
+
+      elif  numPixels.x() < 0 :
+        self.modelPos.m_z -= self.ZOOM
+      self.update() 
+
+    def keyPressEvent(self, event) :
+      key=event.key()
+      if key==Qt.Key.Key_Escape :
+        exit()
+      elif key==Qt.Key.Key_W :
+        glPolygonMode(GL_FRONT_AND_BACK,GL_LINE)
+      elif key==Qt.Key.Key_S :
+        glPolygonMode(GL_FRONT_AND_BACK,GL_FILL)
+      elif key==Qt.Key.Key_Space :
+        self.spinXFace=0
+        self.spinYFace=0
+        self.modelPos.set(Vec3.zero())
+      elif key==Qt.Key.Key_L :
+        self.transformLight^=True      
+      self.update()
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -207,7 +310,12 @@ if __name__ == '__main__':
   format.setSamples(4) 
   format.setMajorVersion(4) 
   format.setMinorVersion(1) 
-  format.setProfile(QSurfaceFormat.CoreProfile) 
+  print(format.profile())
+  if PyQtVersion == 5 :
+    format.setProfile(QSurfaceFormat.CoreProfile) 
+  else :
+    format.setProfile(QSurfaceFormat.OpenGLContextProfile.CoreProfile) 
+
   # now set the depth buffer to 24 bits
   format.setDepthBufferSize(24) 
   # set that as the default format for all windows
@@ -217,4 +325,7 @@ if __name__ == '__main__':
   window.setFormat(format)
   window.resize(1024,720)
   window.show()
-  sys.exit(app.exec_())
+  if PyQtVersion == 5 :
+    sys.exit(app.exec_())
+  else :
+    sys.exit(app.exec())
--- src/NGLSceneMouseControls.cpp
+++ src/NGLSceneMouseControls.cpp
@@ -83,3 +83,4 @@ void NGLScene::wheelEvent( QWheelEvent* _event )
   }
   update();
 }
+  
