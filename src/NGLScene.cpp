4d5e84941a09a07646cc4ac143d03c1d4f012165
jmacey <jmacey@bournemouth.ac.uk>
2022-11-03 11:34:49 +0000

    removed the python demos from the main branch as they are now in the seperate PyNGLDemos repo

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -6,28 +6,24 @@
 #include <QGuiApplication>
 #include <QMouseEvent>
 
-
 NGLScene::NGLScene()
 {
-  setTitle( "Qt5 Simple NGL Demo" );
+  setTitle("Qt5 Simple NGL Demo");
 }
-
 
 NGLScene::~NGLScene()
 {
   std::cout << "Shutting down NGL, removing VAO's and Shaders\n";
 }
 
-
-void NGLScene::resizeGL( int _w, int _h )
+void NGLScene::resizeGL(int _w, int _h)
 {
-  m_projection=ngl::perspective( 45.0f, static_cast<float>( _w ) / _h, 0.1f, 200.0f );
+  m_projection = ngl::perspective(45.0f, static_cast<float>(_w) / _h, 0.1f, 200.0f);
 
-  m_win.width  = static_cast<int>( _w * devicePixelRatio() );
-  m_win.height = static_cast<int>( _h * devicePixelRatio() );
+  m_win.width = static_cast<int>(_w * devicePixelRatio());
+  m_win.height = static_cast<int>(_h * devicePixelRatio());
 }
 constexpr auto shaderProgram = "PBR";
-
 
 void NGLScene::initializeGL()
 {
@@ -36,66 +32,64 @@ void NGLScene::initializeGL()
   ngl::NGLInit::initialize();
   // uncomment this line to make ngl less noisy with debug info
   // ngl::NGLInit::instance()->setCommunicationMode( ngl::CommunicationMode::NULLCONSUMER);
-  glClearColor( 0.4f, 0.4f, 0.4f, 1.0f ); // Grey Background
+  glClearColor(0.4f, 0.4f, 0.4f, 1.0f); // Grey Background
   // enable depth testing for drawing
-  glEnable( GL_DEPTH_TEST );
+  glEnable(GL_DEPTH_TEST);
   // enable multisampling for smoother drawing
-  glEnable( GL_MULTISAMPLE );
+  glEnable(GL_MULTISAMPLE);
   // now to load the shader and set the values
   // grab an instance of shader manager
   // in the code create some constexpr
-  constexpr auto vertexShader  = "PBRVertex";
-  constexpr auto fragShader    = "PBRFragment";
+  constexpr auto vertexShader = "PBRVertex";
+  constexpr auto fragShader = "PBRFragment";
   // create the shader program
-  ngl::ShaderLib::createShaderProgram( shaderProgram );
+  ngl::ShaderLib::createShaderProgram(shaderProgram);
   // now we are going to create empty shaders for Frag and Vert
-  ngl::ShaderLib::attachShader( vertexShader, ngl::ShaderType::VERTEX );
-  ngl::ShaderLib::attachShader( fragShader, ngl::ShaderType::FRAGMENT );
+  ngl::ShaderLib::attachShader(vertexShader, ngl::ShaderType::VERTEX);
+  ngl::ShaderLib::attachShader(fragShader, ngl::ShaderType::FRAGMENT);
   // attach the source
-  ngl::ShaderLib::loadShaderSource( vertexShader, "shaders/PBRVertex.glsl" );
-  ngl::ShaderLib::loadShaderSource( fragShader, "shaders/PBRFragment.glsl" );
+  ngl::ShaderLib::loadShaderSource(vertexShader, "shaders/PBRVertex.glsl");
+  ngl::ShaderLib::loadShaderSource(fragShader, "shaders/PBRFragment.glsl");
   // compile the shaders
-  ngl::ShaderLib::compileShader( vertexShader );
-  ngl::ShaderLib::compileShader( fragShader );
+  ngl::ShaderLib::compileShader(vertexShader);
+  ngl::ShaderLib::compileShader(fragShader);
   // add them to the program
-  ngl::ShaderLib::attachShaderToProgram( shaderProgram, vertexShader );
-  ngl::ShaderLib::attachShaderToProgram( shaderProgram, fragShader );
+  ngl::ShaderLib::attachShaderToProgram(shaderProgram, vertexShader);
+  ngl::ShaderLib::attachShaderToProgram(shaderProgram, fragShader);
   // now we have associated that data we can link the shader
-  ngl::ShaderLib::linkProgramObject( shaderProgram );
+  ngl::ShaderLib::linkProgramObject(shaderProgram);
   // and make it active ready to load values
-  ngl::ShaderLib::use(shaderProgram );
- // We now create our view matrix for a static camera
-  ngl::Vec3 from{ 0.0f, 2.0f, 2.0f };
-  ngl::Vec3 to{ 0.0f, 0.0f, 0.0f };
-  ngl::Vec3 up{ 0.0f, 1.0f, 0.0f };
+  ngl::ShaderLib::use(shaderProgram);
+  // We now create our view matrix for a static camera
+  ngl::Vec3 from{0.0f, 2.0f, 2.0f};
+  ngl::Vec3 to{0.0f, 0.0f, 0.0f};
+  ngl::Vec3 up{0.0f, 1.0f, 0.0f};
   // now load to our new camera
-  m_view=ngl::lookAt(from,to,up);
-  ngl::ShaderLib::setUniform( "camPos", from );
+  m_view = ngl::lookAt(from, to, up);
+  ngl::ShaderLib::setUniform("camPos", from);
   // now a light
-  m_lightPos.set( 0.0, 2.0f, 2.0f ,1.0f);
+  m_lightPos.set(0.0, 2.0f, 2.0f, 1.0f);
   // setup the default shader material and light porerties
   // these are "uniform" so will retain their values
-  ngl::ShaderLib::setUniform("lightPosition",m_lightPos.toVec3());
-  ngl::ShaderLib::setUniform("lightColor",400.0f,400.0f,400.0f);
-  ngl::ShaderLib::setUniform("exposure",2.2f);
-  ngl::ShaderLib::setUniform("albedo",0.950f, 0.71f, 0.29f);
+  ngl::ShaderLib::setUniform("lightPosition", m_lightPos.toVec3());
+  ngl::ShaderLib::setUniform("lightColor", 400.0f, 400.0f, 400.0f);
+  ngl::ShaderLib::setUniform("exposure", 2.2f);
+  ngl::ShaderLib::setUniform("albedo", 0.950f, 0.71f, 0.29f);
 
-  ngl::ShaderLib::setUniform("metallic",1.02f);
-  ngl::ShaderLib::setUniform("roughness",0.38f);
-  ngl::ShaderLib::setUniform("ao",0.2f);
-  ngl::VAOPrimitives::createTrianglePlane("floor",20,20,1,1,ngl::Vec3::up());
+  ngl::ShaderLib::setUniform("metallic", 1.02f);
+  ngl::ShaderLib::setUniform("roughness", 0.38f);
+  ngl::ShaderLib::setUniform("ao", 0.2f);
+  ngl::VAOPrimitives::createTrianglePlane("floor", 20, 20, 1, 1, ngl::Vec3::up());
   ngl::ShaderLib::printRegisteredUniforms(shaderProgram);
   ngl::ShaderLib::use(ngl::nglCheckerShader);
-  ngl::ShaderLib::setUniform("lightDiffuse",1.0f,1.0f,1.0f,1.0f);
-  ngl::ShaderLib::setUniform("checkOn",true);
-  ngl::ShaderLib::setUniform("lightPos",m_lightPos.toVec3());
-  ngl::ShaderLib::setUniform("colour1",0.9f,0.9f,0.9f,1.0f);
-  ngl::ShaderLib::setUniform("colour2",0.6f,0.6f,0.6f,1.0f);
-  ngl::ShaderLib::setUniform("checkSize",60.0f);
+  ngl::ShaderLib::setUniform("lightDiffuse", 1.0f, 1.0f, 1.0f, 1.0f);
+  ngl::ShaderLib::setUniform("checkOn", true);
+  ngl::ShaderLib::setUniform("lightPos", m_lightPos.toVec3());
+  ngl::ShaderLib::setUniform("colour1", 0.9f, 0.9f, 0.9f, 1.0f);
+  ngl::ShaderLib::setUniform("colour2", 0.6f, 0.6f, 0.6f, 1.0f);
+  ngl::ShaderLib::setUniform("checkSize", 60.0f);
   ngl::ShaderLib::printRegisteredUniforms(ngl::nglCheckerShader);
-
 }
-
 
 void NGLScene::loadMatricesToShader()
 {
@@ -107,104 +101,98 @@ void NGLScene::loadMatricesToShader()
     ngl::Mat4 M;
   };
 
-   transform t;
-   t.M=m_view*m_mouseGlobalTX;
+  transform t;
+  t.M = m_view * m_mouseGlobalTX;
 
-   t.MVP=m_projection*t.M;
-   t.normalMatrix=t.M;
-   t.normalMatrix.inverse().transpose();
-   ngl::ShaderLib::setUniformBuffer("TransformUBO",sizeof(transform),&t.MVP.m_00);
+  t.MVP = m_projection * t.M;
+  t.normalMatrix = t.M;
+  t.normalMatrix.inverse().transpose();
+  ngl::ShaderLib::setUniformBuffer("TransformUBO", sizeof(transform), &t.MVP.m_00);
   // ngl::msg->addMessage(fmt::format("size {0}",sizeof(transform)));
-   if(m_transformLight)
-   {
-     ngl::ShaderLib::setUniform("lightPosition",(m_mouseGlobalTX*m_lightPos).toVec3());
-
-   }
+  if (m_transformLight)
+  {
+    ngl::ShaderLib::setUniform("lightPosition", (m_mouseGlobalTX * m_lightPos).toVec3());
+  }
 }
 
 void NGLScene::paintGL()
 {
-  glViewport( 0, 0, m_win.width, m_win.height );
+  glViewport(0, 0, m_win.width, m_win.height);
   // clear the screen and depth buffer
-  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   // grab an instance of the shader manager
   ngl::ShaderLib::use("PBR");
 
   // Rotation based on the mouse position for our global transform
-  ngl::Mat4 rotX;
-  ngl::Mat4 rotY;
-  // create the rotation matrices
-  rotX.rotateX( m_win.spinXFace );
-  rotY.rotateY( m_win.spinYFace );
+  auto rotX = ngl::Mat4::rotateX(m_win.spinXFace);
+  auto rotY = ngl::Mat4::rotateY(m_win.spinYFace);
+
   // multiply the rotations
   m_mouseGlobalTX = rotX * rotY;
   // add the translations
-  m_mouseGlobalTX.m_m[ 3 ][ 0 ] = m_modelPos.m_x;
-  m_mouseGlobalTX.m_m[ 3 ][ 1 ] = m_modelPos.m_y;
-  m_mouseGlobalTX.m_m[ 3 ][ 2 ] = m_modelPos.m_z;
+  m_mouseGlobalTX.m_m[3][0] = m_modelPos.m_x;
+  m_mouseGlobalTX.m_m[3][1] = m_modelPos.m_y;
+  m_mouseGlobalTX.m_m[3][2] = m_modelPos.m_z;
   // get the VBO instance and draw the built in teapot
   // draw
   loadMatricesToShader();
-  ngl::VAOPrimitives::draw( "teapot" );
+  ngl::VAOPrimitives::draw("teapot");
   ngl::ShaderLib::use(ngl::nglCheckerShader);
-  ngl::Mat4 tx;
-  tx.translate(0.0f,-0.45f,0.0f);
-  ngl::Mat4 MVP=m_projection*m_view*m_mouseGlobalTX*tx;
-  ngl::Mat3 normalMatrix=m_view*m_mouseGlobalTX;
+  auto tx = ngl::Mat4::translate(0.0f, -0.45f, 0.0f);
+  auto MVP = m_projection * m_view * m_mouseGlobalTX * tx;
+  ngl::Mat3 normalMatrix = m_view * m_mouseGlobalTX;
   normalMatrix.inverse().transpose();
-  ngl::ShaderLib::setUniform("MVP",MVP);
-  ngl::ShaderLib::setUniform("normalMatrix",normalMatrix);
-  if(m_transformLight)
+  ngl::ShaderLib::setUniform("MVP", MVP);
+  ngl::ShaderLib::setUniform("normalMatrix", normalMatrix);
+  if (m_transformLight)
   {
-    ngl::ShaderLib::setUniform("lightPosition",(m_mouseGlobalTX*m_lightPos).toVec3());
-
+    ngl::ShaderLib::setUniform("lightPosition", (m_mouseGlobalTX * m_lightPos).toVec3());
   }
   ngl::VAOPrimitives::draw("floor");
-
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-void NGLScene::keyPressEvent( QKeyEvent* _event )
+void NGLScene::keyPressEvent(QKeyEvent *_event)
 {
   // that method is called every time the main window recives a key event.
   // we then switch on the key value and set the camera in the GLWindow
-  switch ( _event->key() )
+  switch (_event->key())
   {
-    // escape key to quit
-    case Qt::Key_Escape:
-      QGuiApplication::exit( EXIT_SUCCESS );
-      break;
+  // escape key to quit
+  case Qt::Key_Escape:
+    QGuiApplication::exit(EXIT_SUCCESS);
+    break;
 // turn on wirframe rendering
 #ifndef USINGIOS_
-    case Qt::Key_W:
-      glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
-      break;
-    // turn off wire frame
-    case Qt::Key_S:
-      glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
-      break;
+  case Qt::Key_W:
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    break;
+  // turn off wire frame
+  case Qt::Key_S:
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    break;
 #endif
-    // show full screen
-    case Qt::Key_F:
-      showFullScreen();
-      break;
-    // show windowed
-    case Qt::Key_N:
-      showNormal();
-      break;
-    case Qt::Key_Space :
-      m_win.spinXFace=0;
-      m_win.spinYFace=0;
-      m_modelPos.set(ngl::Vec3::zero());
+  // show full screen
+  case Qt::Key_F:
+    showFullScreen();
+    break;
+  // show windowed
+  case Qt::Key_N:
+    showNormal();
+    break;
+  case Qt::Key_Space:
+    m_win.spinXFace = 0;
+    m_win.spinYFace = 0;
+    m_modelPos.set(ngl::Vec3::zero());
     break;
 
-    case Qt::Key_L :
-    m_transformLight^=true;
+  case Qt::Key_L:
+    m_transformLight ^= true;
     break;
-    default:
-      break;
+  default:
+    break;
   }
   update();
 }
